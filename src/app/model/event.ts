7c9bfe841ba1aac2621cdc3dc18943f08a424151
abjorklund <bjorklunddev@gmail.com>
2019-10-28 14:27:24 +0100

    Now using Observable again

--- src/app/model/event.ts
+++ src/app/model/event.ts
@@ -1,4 +1,11 @@
 export interface Event {
     id: number;
     name: string;
+    description: string;
+    eventStart: Date;
+    eventEnd: Date;
+    deadlineRSVP: Date;
+    location: string;
+    //Partipants: Participant[],
+    organizer: string;
 }
--- src/app/service/event.service.ts
+++ src/app/service/event.service.ts
@@ -13,7 +13,18 @@ export class EventService {
 
   public findById(id: number): Observable<Event> {
       //return this.http.get<User[]>(this.usersUrl);
-    let event = {id:id, name: 'Konferens 2020'};
+    let event:Event = {
+      id:id, 
+      name: 'Konferens 2020',
+      description: 'VA',
+      eventStart: new Date('2020-03-20'),
+      eventEnd: new Date('2020-03-21'),
+      deadlineRSVP: new Date('2020-03-10'),
+      location: "Sierra Nevada motherfucker",
+      organizer: "Knowit" 
+    };
+    
     return of(event);
-    }
+    
+  }
 }
