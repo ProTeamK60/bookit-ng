43dc8c5bf6ad2223b0e3f6d93325d7b767946b2e
Albin Friedner <albin.friedner@knowit.se>
2019-10-29 11:14:19 +0100

    Added a test for the real HTTP call

--- src/app/service/event.service.spec.ts
+++ src/app/service/event.service.spec.ts
@@ -1,12 +1,32 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { EventService } from './event.service';
-import {HttpClientModule} from '@angular/common/http';
+import {EventService} from './event.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {htmlAstToRender3Ast} from '@angular/compiler/src/render3/r3_template_transform';
+import {Event} from '../model/event';
+import {of} from 'rxjs';
 
 describe('EventService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientModule]
-  }));
+  const MOCK_EVENT: Event = {
+    id: 55,
+    name: 'Konferens',
+    description: 'Konferens för Knowit 2020',
+    eventStart: new Date(2020, 3, 20),
+    eventEnd: new Date(2020, 3, 23),
+    deadlineRSVP: new Date(2020, 2, 1),
+    location: 'Sierra Nevada',
+    organizer: 'Susanne'
+  };
+  let httpMock: HttpTestingController;
+  const URL = 'http://localhost:8080/api/v1/events/';
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+
+    httpMock = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
     const service: EventService = TestBed.get(EventService);
@@ -18,5 +38,17 @@ describe('EventService', () => {
     service.findById(55, false).subscribe(data => {
       expect(data.id).toEqual(55);
     });
+  });
+
+  it('should do the real shit', () => {
+    const service: EventService = TestBed.get(EventService);
+    service.findById(55, true).subscribe(data => {
+      expect(data.id).toEqual(55);
+    });
+
+    const findEventRequest = httpMock.expectOne(URL + 55);
+    findEventRequest.flush(MOCK_EVENT);
+
+    httpMock.verify();
   });
 });
--- src/app/service/event.service.ts
+++ src/app/service/event.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Event } from '../model/event'
-import { of } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Event} from '../model/event';
+import {of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,24 +14,23 @@ export class EventService {
 
   constructor(private client: HttpClient) {
     this.eventsUrl = 'http://localhost:8080/api/v1/events/';
-   }
+  }
 
   public findById(id: number, realShit: boolean): Observable<Event> {
-      if (realShit) {
-        return this.client.get<Event>(this.eventsUrl + id);
-      } else {
-        const event: Event = {
-          id: id,
-          name: 'Konferens 2020',
-          description: 'VA',
-          eventStart: new Date('2020-03-20'),
-          eventEnd: new Date('2020-03-21'),
-          deadlineRSVP: new Date('2020-03-10'),
-          location: 'Sierra Nevada motherfucker',
-          organizer: 'Knowit'
-        };
-
-        return of(event);
-      }
+    if (realShit) {
+      return this.client.get<Event>(this.eventsUrl + id);
+    } else {
+      const event: Event = {
+        id: id,
+        name: 'Konferens 2020',
+        description: 'VA',
+        eventStart: new Date('2020-03-20'),
+        eventEnd: new Date('2020-03-21'),
+        deadlineRSVP: new Date('2020-03-10'),
+        location: 'Sierra Nevada motherfucker',
+        organizer: 'Knowit'
+      };
+      return of(event);
+    }
   }
 }
