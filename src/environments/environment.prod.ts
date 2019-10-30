2a8a546fa73e52a1044c86f763e384752c0d10d2
Albin Friedner <albin.friedner@knowit.se>
2019-10-30 14:11:04 +0100

    Optimized docker image build by using .dockerignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  eventServiceAddress: 'http://bookit-event:8080'
+  /** TODO: This address should be updated to http://bookit-event:8080 when we have a better solution for calling bookit-event from the browser */
+  eventServiceAddress: 'http://localhost:8080'
 };
