eb2b41c458bcff8c829d1d8d354a88af9f46d2d3
Ali Afshar <ali.afshar@knowit.se>
2019-12-19 10:52:07 +0100

    change urls for services bookit-event & bookit-registration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
   /** TODO: This address should be updated to http://bookit-event:8080 when we have a better solution for calling bookit-event from the browser */
-  eventServiceAddress: 'https://bzuen1lidj.execute-api.eu-north-1.amazonaws.com/dev',
+  eventServiceAddress: ' https://glhudxfokg.execute-api.us-east-2.amazonaws.com/dev',
+  
 
   /** TODO: This address should be updated to http://bookit-registration:8080 when we have a better solution for calling bookit-registration from the browser */
-  registrationServiceAddress: 'https://bzuen1lidj.execute-api.eu-north-1.amazonaws.com/dev'
+  registrationServiceAddress: 'https://glhudxfokg.execute-api.us-east-2.amazonaws.com/dev'
 };
