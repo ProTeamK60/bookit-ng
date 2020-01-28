d48d4e30fc10940414861e6cc78b41c81f8c9c6f
Christian Vazques <christian.vazques@knowit.se>
2020-01-28 09:23:24 +0100

    code clean up, replaced for with streams and switches with ifs

--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -14,89 +14,52 @@ import { Answer } from '../model/answer';
 export class DynamicFormComponent implements OnInit {
   @Input() event: Event;
   regForm: FormGroup;
-  payLoad = '';
 
   constructor(private registrationService: RegistrationService,
     private formBuilder: FormBuilder,
     private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    console.error(this.event);
     this.regForm = new FormGroup({});
     this.regForm.addControl("email", new FormControl("", [Validators.required, Validators.email]));
 
     this.event.options.forEach(o => {
       let optionForm = {};
-      switch (o.optionType) {
-        case "multiOption":
-          for (let str of o.queryString.split(',')) {
-            optionForm[str] = [false, []];
-          }
-          break;
-        case "oneOption":  
-        case "freeText":
+      if(o.optionType === "multiOption") {
+          o.queryString.split(",").forEach(query => optionForm[query] = [false, []]);
+      } else {
           optionForm["value"] = ['', []];
-          break;
       }
-      let optionValuesGroup: FormGroup = this.formBuilder.group(optionForm);
       let optionDefGroup: FormGroup = new FormGroup({});
-      optionDefGroup.addControl("values", optionValuesGroup);
+      optionDefGroup.addControl("values", this.formBuilder.group(optionForm));
       optionDefGroup.addControl("optionType", new FormControl(o.optionType,[]));
-      //optionForm["optionType"] = [o.optionType,[]];
       this.regForm.addControl(String(o.optionId), optionDefGroup);
     });
-    //this.regForm = this.formBuilder.group(group);
   }
 
 
   onSubmit() {
-    this.payLoad = JSON.stringify(this.regForm.value);
-    let payLoadObj = JSON.parse(this.payLoad);
-    console.error(payLoadObj);
-    let keys = Object.keys(payLoadObj);
+    let formValues = this.regForm.value;
+    let keys = Object.keys(formValues);
     let registration: Registration = {
       eventId: this.activatedRoute.snapshot.params.eventId,
-      participant: {email: payLoadObj["email"],
+      participant: {email: formValues["email"],
       answers: []},
     };
     
     keys.forEach(key => {
       if(key != "email") {
-        let choice = payLoadObj[key];
-        let optionType = choice["optionType"];
+        let choice = formValues[key];
         let values = choice["values"];
-        let answer: Answer;
-        switch(optionType) {
-          case "oneOption":
-          case "freeText":
-            answer = { optionId: Number(key),
-            value: values["value"]
-            };
-            break;
-          case "multiOption":
-            let vals = Object.keys(values);
-            let str = "";
-            vals.forEach(v => {
-              if(values[v]){
-                if(str != "") str += ",";
-                str += v;
-              }
-            });
-            answer = { optionId: Number(key),
-              value: str
-              };
-            break;
+        let answerValue: string;
+        if(choice["optionType"] === "multiOption") {
+          answerValue = Object.keys(values).filter(v => values[v]).join(",");
+        }else {
+          answerValue = values["value"];
         }
-        registration.participant.answers[registration.participant.answers.length] = answer;
+        registration.participant.answers[registration.participant.answers.length] = {optionId: Number(key), value: answerValue};
       }
     });
     console.error(registration);
-/*
-    const registration: Registration = {
-      eventId: this.activatedRoute.snapshot.params.eventId,
-      participant: {email: this.regForm.get('email').value,
-      answers:  [{}]
-      }};
-      */
   }
 }
